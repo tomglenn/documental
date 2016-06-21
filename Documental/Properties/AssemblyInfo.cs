84e3c1834a0fe5741f7dd9ca9d14f6dc95b0f98c
Tom Glenn <tom.glenn@stormid.com>
2016-06-21 10:24:27 +0100

    Updated assembly info and added nuspec

--- Documental/Properties/AssemblyInfo.cs
+++ Documental/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Documental")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A slick and easy to use Azure DocumentDB helper library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Tom Glenn")]
 [assembly: AssemblyProduct("Documental")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
